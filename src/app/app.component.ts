b867b80cc81acfd55e53cb74d695f2e6016f308b
Said Erdem Öztürk <saiderdemozturk@gmail.com>
2024-07-06 16:05:28 +0300

    Meta title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,6 @@ export class AppComponent implements OnInit{
 
     this.metaService.addTags([
       {name: 'keywords', content: 'Software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
     ]);
     
     
